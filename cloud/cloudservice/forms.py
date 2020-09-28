f8edf2c89e9ae45f5b8119ed29c9c128696aa696
dojun <dojun527@naver.com>
2020-09-28 13:53:10 +0900

    update forms.py

--- cloud/cloudservice/forms.py
+++ cloud/cloudservice/forms.py
@@ -17,31 +17,6 @@ class Resource(forms.Form):
     ]
     
 
-    # lan=[
-    #     ('C', 'C'),
-    #     ('C++', 'C++'),
-    #     ('Java', 'Java'),
-    #     ('Javascript', 'Javascript'),
-    #     ('Python', 'Python'),
-    # ]
-  
-    # flav=[
-    #     ('m1.tiny', 'm1.tiny'),
-    #     ('m1.small', 'm1.small'),
-    #     ('m1.medium', 'm1.medium'),
-    #     ('m1.large', 'm1.large'),
-    #     ('m1.xlarge', 'm1.xlarge'),
-    # ]
-
-    # flavor = forms.ChoiceField(\
-    #     required= True,\
-    #     label='학생 수 Flavor 'ㄴ,\
-    #     initial='none',\
-    #     error_messages={'required':'Please enter language info', 'invalid choice': 'Please select a valid one'},\
-    #     disabled=False,
-    #     choices=flav,
-    # )
-
     image = forms.ChoiceField(\
         required= True,\
         label='운영체제(필수 항목) ',\
@@ -51,14 +26,3 @@ class Resource(forms.Form):
         choices=img,
     )
 
-    # language = forms.ChoiceField(\
-    #     required= True,\
-    #     label='Language ',\
-    #     initial='none',\
-    #     error_messages={'required':'Please enter language info', 'invalid choice': 'Please select a valid one'},\
-    #     disabled=False,
-    #     choices=lan,
-    # )
-    
-    
-    
